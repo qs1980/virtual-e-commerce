c16ce396e3d9f0b22f912d72920251f4afc9a341
XTW <xu18660967551@163.com>
2026-01-07 15:59:44 +0800

    初始提交：微服务项目骨架

---
+++ service/notification-service/src/main/java/org/example/NotificationServiceApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
