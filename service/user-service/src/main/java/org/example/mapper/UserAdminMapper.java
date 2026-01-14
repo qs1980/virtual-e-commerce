5a4faed1847c3d26f3b2adb9cc5a6ebd5d4741a3
XTW <xu18660967551@163.com>
2026-01-14 14:08:09 +0800

    增加流式查询最大值，防止oom

--- service/user-service/src/main/java/org/example/mapper/UserAdminMapper.java
+++ service/user-service/src/main/java/org/example/mapper/UserAdminMapper.java
@@ -2,10 +2,12 @@ package org.example.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.cursor.Cursor;
 import org.example.entity.UserEntity;
 
 @Mapper
 public interface UserAdminMapper extends BaseMapper<UserEntity> {
+    @Options(fetchSize = 100)
     Cursor<UserEntity> scanAll();
 }
